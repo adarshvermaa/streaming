d3436ab30e54317ee7737c4b05ba0cefb6527f13
Adarsh Verma <adarshverma.nilbspr@gmail.com>
2025-01-17 00:42:08 +0530

    adding ci/cd pipline

--- server/src/main/routes/users/v1/users.route.ts
+++ server/src/main/routes/users/v1/users.route.ts
@@ -2,16 +2,16 @@ import { Router, Request, Response } from 'express';
 import registerController from '../../../controllers/users_controller/user.controller';
 import { authrization } from '../../../../middileware/auth.middlerware';
 import { zodValidator } from '../../../../middileware/zod.middleware';
-import { userRegisterSchema, userUpdateSchema } from '../../../zod_schemas/users_schemas/user.schemas';
+import { userForgetPasswordSchema, userLoginSchema, userRegisterSchema, userResetPasswordSchema, userUpdateSchema } from '../../../zod_schemas/users_schemas/user.schemas';
 
 const router = Router();
 
 // Define route for /users
 router.post('/', zodValidator(userRegisterSchema), registerController.register);
 router.get('/email-veryfication', registerController.veryficationsEmail);
-router.post('/forget-password', registerController.forgetPassword);
-router.post('/reset-password', registerController.resetPassword);
-router.post('/login', registerController.loginUser);
+router.post('/forget-password', zodValidator(userForgetPasswordSchema), registerController.forgetPassword);
+router.post('/reset-password', zodValidator(userResetPasswordSchema), registerController.resetPassword);
+router.post('/login', zodValidator(userLoginSchema), registerController.loginUser);
 router.put('/user-update', authrization, zodValidator(userUpdateSchema), registerController.updateUser);
 
 export default router;
