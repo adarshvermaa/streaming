1fd16e0c68224912ee63c920b9c800e872ac1f76
Adarsh Verma <adarshverma.nilbspr@gmail.com>
2025-02-02 15:52:20 +0530

    adding synkro app

--- server/src/main/services/synkro/workspace_member.services.ts
+++ server/src/main/services/synkro/workspace_member.services.ts
@@ -1,83 +1,84 @@
-import { WorkspaceMembersModel } from "../../models/synkro/workspace_members.model";
-import { IWorkspaceMember } from "../../type/users/synkro/workspaces_member.type";
+// import { WorkspaceMembersModel } from "../../models/synkro/workspace_members.model";
+// import { IWorkspaceMember } from "../../type/users/synkro/workspaces_member.type";
+// import db  from "../../../config/database/database";
 
-export const createWorkspaceMemberServices = async (
-  workspaceMember: IWorkspaceMember
-): Promise<IWorkspaceMember> => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const [newWorkspaceMember] = await db
-        .insert(WorkspaceMembersModel)
-        .values(workspaceMember)
-        .returning();
-      resolve(newWorkspaceMember);
-    } catch (error) {
-      console.error("Error in createWorkspaceMember:", error);
-      reject(error);
-    }
-  });
-};
-export const updateWorkspaceMemberServices = async (
-  workspaceMember: IWorkspaceMember
-): Promise<IWorkspaceMember> => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const [newWorkspaceMember] = await db
-        .insert(WorkspaceMembersModel)
-        .values(workspaceMember)
-        .returning();
-      resolve(newWorkspaceMember);
-    } catch (error) {
-      console.error("Error in createWorkspaceMember:", error);
-      reject(error);
-    }
-  });
-};
-export const getWorkspaceMemberServices = async (
-  workspaceMember: IWorkspaceMember
-): Promise<IWorkspaceMember> => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const [newWorkspaceMember] = await db
-        .insert(WorkspaceMembersModel)
-        .values(workspaceMember)
-        .returning();
-      resolve(newWorkspaceMember);
-    } catch (error) {
-      console.error("Error in createWorkspaceMember:", error);
-      reject(error);
-    }
-  });
-};  
-export const getAllWorkspaceMemberServices = async (
-  workspaceMember: IWorkspaceMember
-): Promise<IWorkspaceMember> => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const [newWorkspaceMember] = await db
-        .insert(WorkspaceMembersModel)
-        .values(workspaceMember)
-        .returning();
-      resolve(newWorkspaceMember);
-    } catch (error) {
-      console.error("Error in createWorkspaceMember:", error);
-      reject(error);
-    }
-  });
-};
-export const deleteWorkspaceMemberServices = async (
-  workspaceMember: IWorkspaceMember
-): Promise<IWorkspaceMember> => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const [newWorkspaceMember] = await db
-        .insert(WorkspaceMembersModel)
-        .values(workspaceMember)
-        .returning();
-      resolve(newWorkspaceMember);
-    } catch (error) {
-      console.error("Error in createWorkspaceMember:", error);
-      reject(error);
-    }
-  });
-};
+// export const createWorkspaceMemberServices = async (
+//   workspaceMember: IWorkspaceMember
+// ): Promise<IWorkspaceMember> => {
+//   return new Promise(async (resolve, reject) => {
+//     try {
+//       const [newWorkspaceMember] = await db
+//         .insert(WorkspaceMembersModel)
+//         .values(workspaceMember)
+//         .returning();
+//       resolve(newWorkspaceMember);
+//     } catch (error) {
+//       console.error("Error in createWorkspaceMember:", error);
+//       reject(error);
+//     }
+//   });
+// };
+// export const updateWorkspaceMemberServices = async (
+//   workspaceMember: IWorkspaceMember
+// ): Promise<IWorkspaceMember> => {
+//   return new Promise(async (resolve, reject) => {
+//     try {
+//       const [newWorkspaceMember] = await db
+//         .insert(WorkspaceMembersModel)
+//         .values(workspaceMember)
+//         .returning();
+//       resolve(newWorkspaceMember);
+//     } catch (error) {
+//       console.error("Error in createWorkspaceMember:", error);
+//       reject(error);
+//     }
+//   });
+// };
+// export const getWorkspaceMemberServices = async (
+//   workspaceMember: IWorkspaceMember
+// ): Promise<IWorkspaceMember> => {
+//   return new Promise(async (resolve, reject) => {
+//     try {
+//       const [newWorkspaceMember] = await db
+//         .insert(WorkspaceMembersModel)
+//         .values(workspaceMember)
+//         .returning();
+//       resolve(newWorkspaceMember);
+//     } catch (error) {
+//       console.error("Error in createWorkspaceMember:", error);
+//       reject(error);
+//     }
+//   });
+// };  
+// export const getAllWorkspaceMemberServices = async (
+//   workspaceMember: IWorkspaceMember
+// ): Promise<IWorkspaceMember> => {
+//   return new Promise(async (resolve, reject) => {
+//     try {
+//       const [newWorkspaceMember] = await db
+//         .insert(WorkspaceMembersModel)
+//         .values(workspaceMember)
+//         .returning();
+//       resolve(newWorkspaceMember);
+//     } catch (error) {
+//       console.error("Error in createWorkspaceMember:", error);
+//       reject(error);
+//     }
+//   });
+// };
+// export const deleteWorkspaceMemberServices = async (
+//   workspaceMember: IWorkspaceMember
+// ): Promise<IWorkspaceMember> => {
+//   return new Promise(async (resolve, reject) => {
+//     try {
+//       const [newWorkspaceMember] = await db
+//         .insert(WorkspaceMembersModel)
+//         .values(workspaceMember)
+//         .returning();
+//       resolve(newWorkspaceMember);
+//     } catch (error) {
+//       console.error("Error in createWorkspaceMember:", error);
+//       reject(error);
+//     }
+//   });
+// };
