d0b4edb11b4bf0339bd70687f7c7cb3cd6db7c05
Adarsh Verma <adarshverma.nilbspr@gmail.com>
2025-02-05 05:06:20 +0530

    adding more api in workshop memmer

--- server/src/main/routes/synkro/v1/channels.route.ts
+++ server/src/main/routes/synkro/v1/channels.route.ts
@@ -1,10 +1,8 @@
-
-
 import { Router } from "express";
 
 const ChannelsRouter = Router();
 
-ChannelsRouter.get('/', (req, res) => {
-    res.send('Hello World');
+ChannelsRouter.get("/", (req, res) => {
+  res.send("Hello World");
 });
 export default ChannelsRouter;
